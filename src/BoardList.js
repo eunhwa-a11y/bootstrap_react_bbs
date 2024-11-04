1687cc39e78e15ded2e9935917ea754cc30df032
eunhwa-a11y <ttab136@gmail.com>
2024-11-04 15:11:22 +0900

    Write 함수 추가

--- src/BoardList.js
+++ src/BoardList.js
@@ -1,9 +1,63 @@
 import React, { Component } from "react";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
+import Axios from "axios";
+
+/*
+const submitText = () => {
+  // react가 서버로 요청을 보내고, 그 결과를 출력
+  // 지정된 ID를 가진 유저에 대한 요청
+  Axios.get("http://localhost:8000/") // 요청을 보냄
+    .then(function (response) {
+      // 성공 핸들링
+      alert("등록 완료!"); // 요청의 결과 출력
+      console.log(response);
+    })
+    .catch(function (error) {
+      // 에러 핸들링
+      console.log(error);
+    });
+};
+*/
+
+class Board extends Component {
+  render() {
+    return (
+      <tr>
+        <td>1</td>
+        <td>{this.props.title}</td>
+        <td>{this.props.registerId}</td>
+        <td>{this.props.date}</td>
+      </tr>
+    );
+  }
+}
 
 export default class BoardList extends Component {
+  state = {
+    BoardList: [],
+  }
+  getList = () => {
+    Axios.get("http://localhost:8000/list") // 요청을 보냄
+      .then((res) => {
+        // const data = res.data; 아래처럼 작성한 것 과 같음
+        const { data } = res; // destructuring 비구조 할당
+        this.setState({
+          BoardList: data,
+        });
+        console.log(data);
+      })
+      .catch((e) => {
+        // 에러 핸들링
+        console.log(e);
+      });
+  };
+  componentDidMount() {
+    this.getList();
+  }
+
   render() {
+    console.log(this.state.BoardList);
     return (
       <>
         <Table striped bordered hover>
@@ -16,24 +70,14 @@ export default class BoardList extends Component {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>안녕하세요</td>
-              <td>admin</td>
-              <td>2024-11-04</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>안녕하세요</td>
-              <td>admin</td>
-              <td>2024-11-04</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>안녕하세요</td>
-              <td>admin</td>
-              <td>2024-11-04</td>
-            </tr>
+            {this.state.BoardList.map((item) => (
+              <Board
+                key={item.BOARD_ID}
+                title={item.BOARD_TITLE}
+                registerId={item.REGISTER_ID}
+                date={item.REGISTER_DATE}
+              />
+            ))}
           </tbody>
         </Table>
         <div className="d-flex gap-1">
--- src/Write.js
+++ src/Write.js
@@ -1,25 +1,50 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Axios from "axios";
 
 export default class Write extends Component {
+  // Update.js를 만들지 않고 수정할 때 사용 예정
+  state = {
+    isModifyMode: false,
+    title: "",
+    content: "",
+  };
+  write = (e) => {
+    e.preventDefault();
+    Axios.post('http://localhost:8000/insert', {
+      title: this.state.title,
+      content: this.state.content
+    }) // 요청을 보냄
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        // 에러 핸들링
+        console.log(e);
+      });
+  };
+  handleChange = (e) => {
+    this.setState({
+      // title: e.target.value
+      [e.target.name]:e.target.value // 계산된 속성
+    })
+    console.log(this.state);
+  }
+
   render() {
     return (
       <Form>
         <Form.Group className="mb-3" controlId="title">
           <Form.Label>제목</Form.Label>
-          <Form.Control
-            type="text"
-            name="title"
-            placeholder="제목을 입력하세요."
-          />
+          <Form.Control type="text" name="title" placeholder="제목을 입력하세요." onChange={this.handleChange} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="content">
           <Form.Label>내용</Form.Label>
-          <Form.Control as="textarea" name="content" rows={3} />
+          <Form.Control as="textarea" name="content" rows={3} onChange={this.handleChange} />
         </Form.Group>
         <div className="d-flex gap-1">
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" onClick={this.write}>
             작성 완료
           </Button>
           <Button variant="secondary" type="reset">
