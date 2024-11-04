4e0b1b2aa3fc7b85466fcc2d85021436bfc7d6b5
eunhwa-a11y <ttab136@gmail.com>
2024-11-04 10:36:12 +0900

    Write.js import

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Button from "react-bootstrap/Button";
 import "./App.css";
 
 import BoardList from "./BoardList";
+import Write from "./Write";
 
 function App() {
   return (
     <div className="container mt-3">
       <h1 className="mb-3">React Board</h1>
       <BoardList />
+      <Write />
     </div>
   );
 }
